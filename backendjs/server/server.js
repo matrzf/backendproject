2f623ea98b7802bdc005ebb4c55b567d144ade68
JoJoDort <150462574+JoJoDort@users.noreply.github.com>
2023-12-27 23:46:22 +0100

    Update server.js

--- backendjs/server/server.js
+++ backendjs/server/server.js
@@ -1,7 +1,19 @@
-const express = require('express');
+const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json());
+
+app.get("/",(req, res, next)=>{
+  res.send("Bonjour"+JSON.stringify(req.query.text));
+});
+
+app.post("/", (req, res, next) => {
+  res.send("Un mari aimant" + JSON.stringify(req.body));
+});
+
+
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
