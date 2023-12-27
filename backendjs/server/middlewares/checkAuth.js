b091ca137acd202047d173764e5b4c22904c2935
matrzf <104525255+matrzf@users.noreply.github.com>
2023-12-27 18:12:49 +0100

    Update checkAuth.js

--- backendjs/server/middlewares/checkAuth.js
+++ backendjs/server/middlewares/checkAuth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 module.exports = ({ transient = false } = {}) =>
   async function checkAuth(req, res, next) {
